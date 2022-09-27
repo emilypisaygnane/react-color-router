16ddee9e722a4573c9b6507a5ba2ef0d2f6c46db
Emily Pisaygnane <episaygnane@me.com>
2022-09-27 15:55:50 -0700

    added 404 page as well as styling for the 404 page, the navbar and the fullscreen feature

---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,10 @@
+import 'animate.css';
+import './NotFound.css';
+
+export default function NotFound() {
+  return (
+    <div className="not-found">
+      <h1 className="animate__animated animate__pulse">Oops!</h1>
+    </div>
+  );
+}
