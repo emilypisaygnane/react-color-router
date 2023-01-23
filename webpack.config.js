2853d24afe60c7acfd95fdc78bf247f92f3ccdd6
Emily Pisaygnane <episaygnane@me.com>
2023-01-23 11:44:47 -0800

    added webpack to config file, and css loader, created index file in src

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,49 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = { 
-  entry: path.join(__dirname, 'src', 'index.js'),
+  mode: process.env.NODE_ENV === 'productions' ? 'production' : 'development',
+  entry: [
+    './src/index.js',
+  ],
   output: {
-    path:path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    path: __dirname + '/dist',
+    filename: 'app.bundle.js',
+    clean: true,
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: { 
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader' },
+        ],
+      }
+    ]
   },
   plugins: [ 
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'index.html'),
+    }),
+    new webpack.ProvidePlugin({
+      React: 'react',
     }),
   ],
 };
